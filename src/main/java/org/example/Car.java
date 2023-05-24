ab50bfa5a9c9543c3a90f517b36e3cf89007dc6a
Olteanu Alexandra <alexandraolteanu2005@gmail.com>
2023-05-24 23:42:03 +0300

    studied inheritance

--- src/main/java/org/example/Car.java
+++ src/main/java/org/example/Car.java
@@ -1,16 +1,12 @@
 package org.example;
 
-public class Car {
+public class Car extends Vehicle {
     // instance variables
-    String name;
-    double fuelLevel;
-    double mileage;
-    double totalTraveledDistance;
-    double maxSpeed;
+
 
     int doorCount;
-    boolean damaged;
-    String color;
+
     //has a relationship (aggretation)
     Engine engine;
+
 }
---
+++ src/main/java/org/example/Vehicle.java
@@ -0,0 +1,11 @@
+package org.example;
+// "is-a" relationship (inheritance)
+public class Vehicle {
+    String name;
+    double fuelLevel;
+    double mileage;
+    double totalTraveledDistance;
+    double maxSpeed;
+    boolean damaged;
+    String color;
+}
