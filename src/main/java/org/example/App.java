d2391c1d775760fa9367b01aaed31bc2b8c2da15
Olteanu Alexandra <alexandraolteanu2005@gmail.com>
2023-05-24 23:14:21 +0300

    added Car class; studied variables

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -46,6 +46,13 @@ public class App
         System.out.println("Name of new car: " + carReference.name);
         carReference.name = "BMW";
 
+        carReference = car2;
+        System.out.println("Name of car from 2 variables. CarReference: " + carReference.name );
+        System.out.println("Name of car from 2 variables. Car2: " + car2.name );
+        car2.name = "Dacia";
+        System.out.println("Updated name of car from 2 variables. CarReference: " + carReference.name );
+        System.out.println("Updated name of car from 2 variables. Car2: " + car2.name );
+
 
 
 
